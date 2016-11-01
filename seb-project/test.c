3a92470ef8ea8391b713d4f172bb76b896d17b9f
Aryeh Katz <in@akatz.io>
2016-11-01 18:55:01 +0200

    project reboot, name error

---
+++ seb-project/test.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+void main(int argc, char *argv[]) {
+
+	printf("C is cool !");
+	return;
+
+}
--- sep-project/test2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
- 
-void main(int argc, char *argv[]) {
- 
-    printf("C is cool !");
-    return;
- 
-}
