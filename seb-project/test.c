29a58ad2601e268d4317cb21e89423fdd265bbe4
Aryeh Katz <in@akatz.io>
2016-11-02 11:13:10 +0200

    reset

--- seb-project/test.c
+++ seb-project/test.c
@@ -5,11 +5,7 @@
 
 void main(int argc, char *argv[]) {
 
-<<<<<<< HEAD
-	printf("eli moshkovich");
-=======
-	printf("MY CHANGE");
->>>>>>> 4d5abd0e475a93a119cedecf3f6c045a6b0624cb
+	
 	return;
 
 }
