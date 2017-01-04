817044f0db25516137e1187a5644980e0a341bac
Hanna_yair <chenyair1617@gmail.com>
2017-01-04 12:21:03 +0200

    new structs Tasks

--- seb-project/lib/structs.h
+++ seb-project/lib/structs.h
@@ -18,3 +18,25 @@ typedef struct user_node {
 	users * ptr;
 	user_node * next;
 }user_node;
+
+
+typedef struct tasks {
+	long id;
+	char * name;
+
+	int user_id;
+	int category_id;
+
+	int salary;
+	bool status;
+	char * tags;
+	char * deadLine;
+
+	int comments[];
+
+}tasks;
+
+typedef struct task_node {
+	tasks * ptr;
+	task_node * next;
+}task_node;
