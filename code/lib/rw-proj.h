be899dfe1858fcad706b0523c5d770ebf34007be
Eli Moshkovich <tbhjfo14@gmail.com>
2017-01-04 16:54:14 +0200

    proj fix

--- code/lib/rw-proj.h
+++ code/lib/rw-proj.h
@@ -2,33 +2,33 @@ void setProjFromLine(struct task ** node, char * line) {
 	char ch;
 	// get ID
 	do {
-		ch = getc(line);
+		ch = getch(line);
 
 	} while (ch != '|');
 
 	// move over the next |
-	getc(line);
+	getch(line);
 
 	// get title
 }
 
 void getProjects() {
-	FILE * tasks_file;
+	FILE * proj_file;
 	char filename[] = "db/tasks.txt";
-	tasks_file = fopen(filename, "r");
+	proj_file = fopen(filename, "r");
 
-	if (tasks_file == NULL) {
+	if (proj_file == NULL) {
 		perror("Error");
 		return;
 	}
 
-	while (!feof(tasks_file)) {
+	while (!feof(proj_file)) {
 		struct task * task_node = (struct task *)malloc(sizeof(task));
-
 		const size_t line_size = 300;
 		char* line = malloc(line_size);
-		fgets(line, line_size, tasks_file);
+		fgets(line, line_size, proj_file);
 		setProjFromLine(&task_node, line);
 		free(line);
 	}
+	fclose(proj_file);
 }
