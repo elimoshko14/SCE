bd671c153917532acd1b8ef868c39573c9df46bf
Aryeh Katz <in@akatz.io>
2017-01-04 13:57:54 +0200

    add globals

--- seb-project/lib/globals.h
+++ seb-project/lib/globals.h
@@ -5,6 +5,10 @@ typedef enum bool{
 }bool;
 
 // singles
-struct user * user_ptr = NULL;
+user * user_ptr = NULL;
 
 // lists
+user_node * users_list = NULL;
+proj_node * projects_list = NULL;
+task_node * tasks_list = NULL;
+comment_node * comments_list = NULL;
