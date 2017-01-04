836473763f40038ef817f14b07c9f71216cec4d0
Aryeh Katz <in@akatz.io>
2017-01-04 11:16:33 +0200

    struct

--- seb-project/app.c
+++ seb-project/app.c
@@ -5,12 +5,13 @@
 #include <string.h>
 #include <time.h>
 
-#include "lib/structs.h"
 #include "lib/globals.h"
+#include "lib/structs.h"
 #include "lib/login.h"
 
+
 int main(int argc, char *argv[]) {
-	user_id = login();
+	 = login();
 
 
 	return;
--- seb-project/lib/globals.h
+++ seb-project/lib/globals.h
@@ -1,3 +1,8 @@
+typedef enum bool{
+	true = 1,
+	false
+} boolian;
+
 // singles
 struct user * user_ptr = NULL;
 
