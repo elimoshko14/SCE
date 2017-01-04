36eaaaff690ede10fb4f72570c860ed28d3db5d5
Aryeh Katz <in@akatz.io>
2017-01-04 14:41:58 +0200

    fix app

--- seb-project/app.c
+++ seb-project/app.c
@@ -5,14 +5,27 @@
 #include <string.h>
 #include <time.h>
 
+/*********************/
+/*	Generic Includes */
+
 #include "lib/globals.h"
 #include "lib/structs.h"
+
+#include "lib/rw-task.h"
+
 #include "lib/login.h"
 
 
+/*********************/
+/*	Unit Testing	 */
+
+#include "unitTesting/getTasks.h"
+
 int main(int argc, char *argv[]) {
 	// init()
-	login();
-	return;
+	// login();
+
+
+	return 0;
 }
 
