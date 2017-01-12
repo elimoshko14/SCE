ef46c3969dd85029b2c959d9b1787f39207affad
Hanna_yair <chenyair1617@gmail.com>
2017-01-12 17:13:57 +0200

    rw comments

--- code/lib/rw-cmnt.h
+++ code/lib/rw-cmnt.h
@@ -1,3 +1,84 @@
+void printComment(comment * node) {
+	printf("id %ld\n", node->id);
+	printf("title %s\n", node->title);
+	printf("body %s\n", node->body);
+	printf("project_id %d\n", node->porj_id);
+	printf("task_id %d\n", node->task_id);
+	printf("user_id %d\n", node->user_id);
+	printf("-------------\n");
+}
+
+void printComentTree() {
+	comment_node * c = comments_list;
+	if (!c)
+		return;
+	while (c)
+	{
+		printTask(c->ptr);
+		c = c->next;
+	}
+}
+
+void pushComent(comment * node)
+{
+	struct comment_node * temp;
+	temp = (struct comment_node *)malloc(sizeof(struct comment_node));
+	temp->ptr = node;
+	if (comments_list == NULL)
+	{
+		comments_list = temp;
+		comments_list->next = NULL;
+	}
+	else
+	{
+		temp->next = comments_list;
+		comments_list = temp;
+	}
+}
+
+bool deleteComent(int id)
+{
+	struct comment_node * temp, *prev = NULL;
+	temp = comments_list;
+	while (temp != NULL)
+	{
+		if (temp->ptr->id == id)
+		{
+			if (temp == comments_list)
+			{
+				comments_list = temp->next;
+				free(temp);
+				return true;
+			}
+			else
+			{
+				prev->next = temp->next;
+				free(temp);
+				return true;
+			}
+		}
+		else
+		{
+			prev = temp;
+			temp = temp->next;
+		}
+	}
+	return false;
+}
+
+comment_node * findcomentById(int id) {
+	struct comment_node * temp;
+	temp = comments_list;
+	while (temp != NULL)
+	{
+		if (temp->ptr->id == id)
+			return temp;
+		else
+			temp = temp->next;
+	}
+	return NULL;
+}
+
 void getComments() {
 
 	FILE * coments_file;
@@ -16,44 +97,29 @@ void getComments() {
 		char buffer[256];
 		// get id
 		fscanf(coments_file, "%ld", &(coment_struct->id));
-		// drop line
 		fgets(buffer, 256, coments_file);
 
 		// get title
 		fgets(coment_struct->title, '256', coments_file);
-		// clear \n
 		strtok(coment_struct->title, "\n");
 
 		// get body
 		fgets(coment_struct->body, '256', coments_file);
-		// clear \n
 		strtok(coment_struct->body, "\n");
 
 		// get proj id
 		fscanf(coments_file, "%d", &(coment_struct->porj_id));
-		// drop line
 		fgets(buffer, 256, coments_file);
 
 		// get task id
 		fscanf(coments_file, "%d", &(coment_struct->task_id));
-		// drop line
 		fgets(buffer, 256, coments_file);
 
 		// get user id
 		fscanf(coments_file, "%d", &(coment_struct->user_id));
-		// drop line
 		fgets(buffer, 256, coments_file);
 
-
-
-		printf("id %ld\n", coment_struct->id);
-		printf("title %s\n", coment_struct->title);
-		printf("body %s\n", coment_struct->body);
-		printf("project_id %d\n", coment_struct->porj_id);
-		printf("task_id %d\n", coment_struct->task_id);
-		printf("user_id %d\n", coment_struct->user_id);
-		printf("-------------\n");
-
+		pushComent(coment_struct);
 		/*******/
 		// FEOF IS BUGGED == TRUE IF END OF FILE ()
 		/*******/
