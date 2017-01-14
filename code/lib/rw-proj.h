761195899666cbdb6bb82aee053c7c758f2905ed
Wer4ty <wer4ty@gmail.com>
2017-01-14 23:26:34 +0200

    test

--- code/lib/rw-proj.h
+++ code/lib/rw-proj.h
@@ -171,7 +171,7 @@ void addProjs() {
 
 	struct proj * proj_struct = (struct proj *)malloc(sizeof(struct proj));
 
-	printf("Enter id: ");
+	/*printf("Enter id: ");
 	scanf("%d", &(proj_struct->id));
 	while (findProjById(proj_struct->id) != NULL) {
 		printf("Error details, try again/n");
@@ -179,11 +179,15 @@ void addProjs() {
 	}
 
 	printf("Enter deadline and cost of project:/n");
-	scanf("%s%d", proj_struct->due, &(proj_struct->cost));
+	scanf("%s%d", proj_struct->due, &(proj_struct->cost));*/
 
-	//just for test
-	proj_struct->manager_id = 305327223;
+
 	// project information is empty for  new project
+	proj_struct->id = 3; // id  have to be auto generate (len(projects_list)+1)
+	proj_struct->manager_id = 305327223;
+	
+	strcpy(proj_struct->due, "21.01.2017");
+	proj_struct->cost = 1000;
 	strcpy(proj_struct->users_arr, "-1");
 	strcpy(proj_struct->tasks_arr, "-1");
 	strcpy(proj_struct->cats_arr, "-1");
@@ -193,6 +197,3 @@ void addProjs() {
 	setProjs(proj_struct);
 
 }
-
-
-
