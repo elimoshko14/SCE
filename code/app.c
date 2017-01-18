6091086244d59474e8a03daffbe94863f629e9eb
Hanna_yair <chenyair1617@gmail.com>
2017-01-18 13:59:57 +0200

    hooo ye'

--- code/app.c
+++ code/app.c
@@ -18,7 +18,7 @@
 #include "lib/rw-ctgry.h"
 #include "lib/rw-cmnt.h"
 //--------------------//
-#include "lib/mainMenu.h"
+#include "modules/mainMenu.h"
 #include "modules/login.h"
 #include "modules/bonus_EEO.h"
 // DO NOT CHANGE BELOW
--- code/modules/mainMenu.h
+++ code/modules/mainMenu.h
@@ -21,20 +21,22 @@ void mainMenu(user *u)
 		break;
 	}
 }
-void menu_1() {
-	printf("enter 1 for privide tastks of workers:\n");
-	printf("enter 2 for Management projetcts:\n");
-	printf("enter 3 for Management tasks:\n");
-	printf("enter 4 for Management users:\n");
-	printf("enter 5 for Management comments:\n");
-	printf("enter 6 for Management categories:\n");
+
+void menu_1() 
+{
+	printf("enter 1 for provide tastks of workers:\n");
+	printf("enter 2 for management projetcts:\n");
+	printf("enter 3 for management tasks:\n");
+	printf("enter 4 for management users:\n");
+	printf("enter 5 for management comments:\n");
+	printf("enter 6 for management categories:\n");
 	printf("enter 7 to get projects list:\n");
 	printf("enter 8 to get users list:\n");
 	printf("enter 9 to get tasks list:\n");
 	printf("enter 10  to get categories list:\n");
 	printf("enter 11 to get comments list:\n");
-	printf("enter 12 for associate task:\n");
-	printf("enter 13 for associate project:\n");
+	printf("enter 12 for changeing associate task:\n");
+	printf("enter 13 for changeing associate project:\n");
 	printf("enter 14 for replication task:\n");
 	printf("enter 15 for replication project:\n");
 	printf("enter 16 for changing the interface language to Russian:\n");
@@ -49,4 +51,14 @@ void menu_1() {
 	default:
 		break;
 	}
+}
+
+void menu_2()
+{
+
+}
+
+void menu_3()
+{
+
 }
