b4ed559cec45643c7d8e90e76604727050bbf869
Hanna_yair <chenyair1617@gmail.com>
2017-01-18 10:41:52 +0200

    =1

--- code/lib/rw-proj.h
+++ code/lib/rw-proj.h
@@ -1,3 +1,16 @@
+proj_node * findProjById(int id) {
+	struct proj_node * temp;
+	temp = projects_list;
+	while (temp != NULL)
+	{
+		if (temp->ptr->id == id)
+			return temp;
+		else
+			temp = temp->next;
+	}
+	return NULL;
+}
+
 void printProj(proj * proj_struct) {
 	printf("ID:\t%d\n", proj_struct->id);
 	printf("Users Array:\t%s\n", proj_struct->users_arr);
@@ -68,19 +81,6 @@ bool deleteProj(int id)
 	return false;
 }
 
-proj_node * findProjById(int id) {
-	struct proj_node * temp;
-	temp = projects_list;
-	while (temp != NULL)
-	{
-		if (temp->ptr->id == id)
-			return temp;
-		else
-			temp = temp->next;
-	}
-	return NULL;
-}
-
 void getProjs() {
 
 	FILE * proj_file;
@@ -99,35 +99,27 @@ void getProjs() {
 		char buffer[256];
 		// get id
 		fscanf(proj_file, "%d", &(proj_struct->id));
-		// end line
 		fgets(buffer, 256, proj_file);
 		// get user arr
 		fscanf(proj_file, "%s", (proj_struct->users_arr));
-		// end line
 		fgets(buffer, 256, proj_file);
 		// get tasks_arr
 		fscanf(proj_file, "%s", proj_struct->tasks_arr);
-		// end line
 		fgets(buffer, 256, proj_file);
 		// get cats_arr
 		fscanf(proj_file, "%s", proj_struct->cats_arr);
-		// drop line
 		fgets(buffer, 256, proj_file);
 		// get manager_id
 		fscanf(proj_file, "%d", &(proj_struct->manager_id));
-		// drop line
 		fgets(buffer, 256, proj_file);
 		// get finish_date
 		fscanf(proj_file, "%s", proj_struct->due);
-		// drop line
 		fgets(buffer, 256, proj_file);
 		// get status
 		fscanf(proj_file, "%d", &(proj_struct->status));
-		// drop line
 		fgets(buffer, 256, proj_file);
 		// get cost
 		fscanf(proj_file, "%d", &(proj_struct->cost));
-		// drop line
 		fgets(buffer, 256, proj_file);
 
 		pushProj(proj_struct);
@@ -165,18 +157,20 @@ void setProjs(proj *node) {
 		node->due,
 		node->status,
 		node->cost);
+
+	fclose(proj_file);
 }
 
-void addProj() {
+void addProj(int id, int manager_id,int cost,char due[]) {
 
 	// project information is empty for  new project
 
 	struct proj * proj_struct = (struct proj *)malloc(sizeof(struct proj));
 
-	proj_struct->id = 3; // id  have to be auto generate (len(projects_list)+1)
-	proj_struct->manager_id = 305327223;
-	strcpy(proj_struct->due, "21.01.2017");
-	proj_struct->cost = 1000;
+	proj_struct->id = id; // id  have to be auto generate (len(projects_list)+1)
+	proj_struct->manager_id = manager_id;
+	strcpy(proj_struct->due, due);
+	proj_struct->cost = cost;
 	strcpy(proj_struct->users_arr, "-1");
 	strcpy(proj_struct->tasks_arr, "-1");
 	strcpy(proj_struct->cats_arr, "-1");
@@ -185,4 +179,49 @@ void addProj() {
 	pushProj(proj_struct);
 	setProjs(proj_struct);
 
+}
+
+void updateProj(int id,int manager_id , char due[],bool status,int cost) {
+	proj *node = findProjById(id);
+	if (node != NULL) {
+		printf("Proj has been find!\n");
+		node->id = id;
+		node->manager_id = manager_id;
+		strcpy(node->users_arr, "-1");
+		strcpy(node->tasks_arr, "-1");
+		strcpy(node->cats_arr, "-1");
+		strcpy(node->due, due);
+		node->status = status;
+		node->cost = cost;
+	}
+	else
+	{
+		printf("proj not find\n");
+	}
+	clearFile("../db/projects.txt");
+
+	proj_node * t = projects_list;
+	while (t) {
+		setProjs(t->ptr);
+		t = t->next;
+
+	}
+}
+
+void unSetProj(int id) {
+
+	// only if previous delete from users_list
+	if (deleteProj(id)) {
+
+		proj_node * new_list = projects_list;
+		if (!new_list) return;
+
+		clearFile("../db/projects.txt");
+
+		// rewrite new list without deleting node
+		while (new_list) {
+			setProjs(new_list->ptr);
+			new_list = new_list->next;
+		}
+	}
 }
--- code/unitTesting/testProj.h
+++ code/unitTesting/testProj.h
@@ -1,5 +1,10 @@
 bool unitTestProj() {
-	printf("----------- Add,delete, update, Project -----------\n\n");
-	addProj(); 
+	printf("----------- Add, delete, update Project -----------\n\n");
+
+	for (int i = 2; i <= 4; i++)
+		addProj(i,1+2, 1000*i, "12.01.2017");
+
+	getProjs();
+	unSetProj(2);
 	printProjTree();
 }
