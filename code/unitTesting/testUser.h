858706f710833bbbb39fe76da57fb0f1461fe756
KatZ <in@akatz.io>
2017-01-18 10:19:20 +0200

    add unit

---
+++ code/unitTesting/testUser.h
@@ -0,0 +1,10 @@
+bool unitTestUser() {
+	printf("----------- Add, delete, update User -----------\n\n");
+	
+	for (int i=2; i<=4; i++) 
+		addUser(i,"User", "EN", "12.01.2017");
+	
+	getUsers();
+	unSetUser(2);
+	printUserTree();
+}
