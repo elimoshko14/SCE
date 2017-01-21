02311c897dad663daefcb3a88dcbdfc08852a556
KatZ <in@akatz.io>
2017-01-21 23:58:14 +0200

    manager menu update

--- code/modules/mainChoose.h
+++ code/modules/mainChoose.h
@@ -23,18 +23,18 @@ void directorChoose(int ch) {
 
 void managerChoose(int ch) {
 	switch (ch) {
-	case 1: { management_projets();  break; }
-	case 2: { displayProjects();  break; }
-	case 3: {  management_category();  break; }
-	case 4: {  displayCategories();  break; }
-	case 5: {  management_tasks(); break; }
-	case 6: {  displayTask(1);  break; }
-	case 7: {  displayTask(0); break; }
-	case 8: {  menageComments(); break; }
-	case 9: {  displayCommentsByTask(); break; }
-	case 10: { displayCommentsByUser(); break; }
-	case 11: {  management_users();  break; }
-	case 12: {  displayUsersbyProject();  break; }
+	case 1: {management_category();  break; }
+	case 2: { displayCategories();  break; }
+	case 3: {  management_tasks();  break; }
+	case 4: {  displayTask(1);  break; }
+	case 5: {  displayTask(0); break; }
+	case 6: {  menageComments();  break; }
+	case 7: {  displayCommentsByTask(); break; }
+	case 8: {  displayCommentsByUser(); break; }
+	case 9: {  management_users(); break; }
+	case 10: { displayUsersbyProject(); break; }
+	case 11: {  displayWorkers();  break; }
+	case 12: {  displayManagers();  break; }
 	case 13: {  printf("\n\n Bye Bye \n\n"); exit(0); break; }
 	default: break;
 	}
