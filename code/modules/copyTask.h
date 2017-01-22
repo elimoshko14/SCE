c8703b0633bc7eb7256aaef90b31bc5d048c0130
Aryeh Katz <in@akatz.io>
2017-01-22 14:37:39 +0200

    fix task

--- code/modules/copyTask.h
+++ code/modules/copyTask.h
@@ -1,4 +1,3 @@
-
 void copyTask() {
 	int copy_id;
 		if (tasks_list) {
@@ -6,7 +5,7 @@ void copyTask() {
 			copy_id = getInt();
 			task *tmpT = findTaskById(copy_id);
 			if (tmpT) {
-				addTask(++unique_task_key, tmpT->title, tmpT->user_id, tmpT->category_id, tmpT->cost, tmpT->status, tmpT->tags, tmpT->due, tmpT->comments);
+				addTask(++unique_task_key, tmpT->title, 0, tmpT->category_id, tmpT->cost, tmpT->status, tmpT->tags, tmpT->due, "");
 				printf("Task has been copy ID\nTo back press enter");
 				getchar(); 
 				getchar();
