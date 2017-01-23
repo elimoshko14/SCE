7c6ddeedc97f61e799bafd24de728a8980e5f9b4
KatZ <in@akatz.io>
2017-01-23 14:44:06 +0200

    impr

--- code/modules/Session.h
+++ code/modules/Session.h
@@ -1,6 +1,6 @@
 void userSession() {
 	system("cls");
-	printf("\n %s %s\n\n",i18("Hello"),  user_ptr->name, i18("welcome to system"));
+	printf("\n %s %s\n %s\n",i18("Hello"),  user_ptr->name, i18("welcome to system"));
 	// print main menu
 	mainMenu();
 
