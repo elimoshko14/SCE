2fdda9f90c3ea795b0bd828bce67709b0e7e9a58
Aryeh Katz <in@akatz.io>
2017-01-23 12:58:48 +0200

    fix 2

--- code/i18/helper.h
+++ code/i18/helper.h
@@ -1,25 +1,46 @@
 #define LEN 1000
 
-char MANAG_PROJECT[LEN];
-char MANAG_USERS[LEN];
-
-
-
+char MANAGE_PROJECT[LEN];
+char MANAGE_USERS[LEN];
 
 
 void changeLang() {
 
 	if (!user_ptr) {
-		strcpy(MANAG_PROJECT, "Manage projects");
+		strcpy(MANAGE_PROJECT, "Manage projects");
 	}
 	else {
-		if (strcmp(user_ptr->lang, "EN")== 0){
-			strcpy(MANAG_PROJECT,"Manage projects");
-			strcpy(MANAG_USERS, "Manage users");
-		}
-		else {
-			strcpy(MANAG_PROJECT, "פûגפצגû ןפûגא פן ןגאûפאûג");
-			strcpy(MANAG_USERS, "פכûהאנרהûנןהûכאגנה");
-		}
+		char i18_ru[] = "../i18/RU.txt";
+		char i18_en[] = "../i18/EN.txt";
+		FILE * file_ptr;
+		if (strcmp(user_ptr->lang, "EN") == 0)
+			file_ptr = fopen(i18_en, "r");
+		else file_ptr = fopen(i18_ru, "r");
+		if (isEmptyFile(file_ptr)) return;
+
+		// GET DATA FROM FILE //
+		char ch; // See end of loop
+		do {
+			char key[LEN], value[LEN];
+			fgets(key, LEN, file_ptr);
+			strtok(key, "\n");
+
+			if (strcmp(key, "MANAGE_PROJECT") == 0) {
+				fgets(value, LEN, file_ptr);
+				strtok(value, "\n");
+				strcpy(MANAGE_PROJECT, value);
+			}
+			else if (strcmp(key, "MANAGE_USERS") == 0) {
+				fgets(value, LEN, file_ptr);
+				strtok(value, "\n");
+				strcpy(MANAGE_USERS, value);
+			}
+			else {}
+			
+			ch = fgetc(file_ptr);
+			ungetc(ch, file_ptr);
+		} while (!feof(file_ptr) && ch != EOF);
+
+		fclose(file_ptr);
 	}
 }
